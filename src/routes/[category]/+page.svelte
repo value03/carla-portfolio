f0aa358a35af7aeb738ce970d80775c8e69ae00d
nimbes <vaklap@ethz.ch>
2025-06-07 12:47:20 +0200

    testing fetch

--- src/routes/[category]/+page.svelte
+++ src/routes/[category]/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	import { project } from '$lib';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
 </script>
 
-<h1>{data.projects}</h1>
+<h1>{await data.projects}</h1>
