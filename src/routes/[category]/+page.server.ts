f2073a1d882ba7d4484479df94f27c90368d2d47
nimbes <vaklap@ethz.ch>
2025-06-09 12:14:58 +0200

    i thing it works

--- src/routes/[category]/+page.server.ts
+++ src/routes/[category]/+page.server.ts
@@ -2,10 +2,10 @@ import type { PageServerLoad } from './$types';
 
 /**  */
 export const load: PageServerLoad = async ({ params, fetch }) => {
+	const res = await fetch('http://localhost:1337/api/projects/');
+
 	return {
 		route: params.category,
-		projects: fetch('http://localhost:1337/api/projects/')
-			.then((response) => response.json())
-			.then((response) => response)
+		projects: await res.json()
 	};
 };
